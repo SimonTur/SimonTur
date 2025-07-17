2eb732d003ee69d5fae36b46523d0a457e6ce039
SimonTur <turkeyevsa@gmail.com>
2025-07-18 01:56:34 +0500

    11V

--- src/main/java/ru/SimonTur/tgBot/service/ProductService.java
+++ src/main/java/ru/SimonTur/tgBot/service/ProductService.java
@@ -5,11 +5,10 @@ import ru.SimonTur.tgBot.model.Product;
 import java.util.List;
 @Service
 public interface ProductService {
+    Product createProduct(Product product);
     List<Product> getProductsByCategoryId(long id);
     List<Product> getClientProducts(long clientId);
     List<Product> getTopPopularProducts(Integer limit);
     List<Product> searchProductsByName(String name);
-        // Добавляем новый метод
-        Product createProduct(Product product);
-       List<Product> getClientProducts(Long clientId);
+    List<Product> getClientProducts(Long clientId);
     }
